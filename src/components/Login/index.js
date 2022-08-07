97c106995db908edced34173a86d38aa1c5ecf73
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2022-08-07 07:20:00 +0000

    input taking functionality added to login form

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,42 +1,72 @@
+import {Component} from 'react'
 import './index.css'
 
-const Login = () => (
-  <div className="bg-container">
-    <form className="form-container">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
-        alt="logo"
-        className="login-logo"
-      />
-      <label className="label" htmlFor="username">
-        USERNAME
-      </label>
-      <input
-        type="text"
-        id="username"
-        className="input-field"
-        placeholder="Username"
-      />
-      <label className="label" htmlFor="password">
-        PASSWORD
-      </label>
-      <input
-        type="password"
-        id="username"
-        className="input-field"
-        placeholder="Password"
-      />
-      <div className="show-password-container">
-        <input type="checkbox" id="showPassword" className="checkbox" />
-        <label className="label checkbox-label" htmlFor="showPassword">
-          Show Password
-        </label>
+class Login extends Component {
+  state = {
+    username: '',
+    password: '',
+  }
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmitForm = event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    console.log({username, password})
+  }
+
+  render() {
+    const {username, password} = this.state
+
+    return (
+      <div className="bg-container">
+        <form className="form-container" onSubmit={this.onSubmitForm}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+            alt="logo"
+            className="login-logo"
+          />
+          <label className="label" htmlFor="username">
+            USERNAME
+          </label>
+          <input
+            type="text"
+            id="username"
+            className="input-field"
+            placeholder="Username"
+            value={username}
+            onChange={this.onChangeUsername}
+          />
+          <label className="label" htmlFor="password">
+            PASSWORD
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="input-field"
+            placeholder="Password"
+            value={password}
+            onChange={this.onChangePassword}
+          />
+          <div className="show-password-container">
+            <input type="checkbox" id="showPassword" className="checkbox" />
+            <label className="label checkbox-label" htmlFor="showPassword">
+              Show Password
+            </label>
+          </div>
+          <button className="custom-button" type="submit">
+            Login
+          </button>
+        </form>
       </div>
-      <button className="custom-button" type="button">
-        Login
-      </button>
-    </form>
-  </div>
-)
+    )
+  }
+}
 
 export default Login
